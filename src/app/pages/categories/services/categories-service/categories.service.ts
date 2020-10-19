5ff7e51ba0480f5777d630a2f5f82029f36fdfaf
Allan Guerra <allang@ciandt.com>
2020-10-19 18:52:03 -0300

    feat: create category service

---
+++ src/app/pages/categories/services/categories-service/categories.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { BASE_URL, endpoints } from '../../../../../environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Category } from 'src/app/models/category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${BASE_URL}/${endpoints.categories}`)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  getById(id: number): Observable<Category> {
+    return this.http.get<Category>(`${BASE_URL}/${endpoints.categories}/${id}`)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  store(category: Category): Observable<Category> {
+    return this.http.post<Category>(`${BASE_URL}/${endpoints.categories}`, category)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  update(category: Category): Observable<Category> {
+    return this.http.put<Category>(`${BASE_URL}/${endpoints.categories}/${category.id}`, category)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${BASE_URL}/${endpoints.categories}/${id}`)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log(`Error on requisition: ${error}`);
+    return throwError(error);
+  }
+}
