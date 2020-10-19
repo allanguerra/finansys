1cf46659c7dda2836d22c74856aad94138704e21
Allan Guerra <allang@ciandt.com>
2020-10-19 17:24:31 -0300

    chore: add cotagories children routes

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { CategoryFormComponent } from './components/category-form/category-form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CategoryListComponent
+  },
+  {
+    path: 'new',
+    component: CategoryFormComponent
+  },
+  {
+    path: ':id/edit',
+    component: CategoryFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
