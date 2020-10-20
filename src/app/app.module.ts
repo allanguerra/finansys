2e215ed9a46f03f4536aaf6baed3e6473d287ac0
Allan Guerra <allan.guerra19@gmail.com>
2020-10-19 23:57:14 -0300

    chore: add ptBr locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr)
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +25,11 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [
-    MessageService
+    MessageService,
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    }
   ],
   bootstrap: [AppComponent]
 })
