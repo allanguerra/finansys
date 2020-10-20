b3b3afce82034b3e43720d2edbd106f5a0c029b0
Allan Guerra <allan.guerra19@gmail.com>
2020-10-19 23:23:07 -0300

    chore: add entries routes

--- src/app/pages/entries/entries-routing.module.ts
+++ src/app/pages/entries/entries-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EntryListComponent } from './components/entry-list/entry-list.component';
+import { EntryFormComponent } from './components/entry-form/entry-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EntryListComponent
+  },
+  {
+    path: 'new',
+    component: EntryFormComponent
+  },
+  {
+    path: ':id/edit',
+    component: EntryFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EntriesRoutingModule } from './entries-routing.module';
+import { EntryListComponent } from './components/entry-list/entry-list.component';
+import { EntryFormComponent } from './components/entry-form/entry-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [EntryListComponent, EntryFormComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     EntriesRoutingModule
   ]
 })
