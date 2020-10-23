8d80f0fc3728194bd2066121faf62b870a484629
Allan Guerra <allan.guerra19@gmail.com>
2020-10-22 22:11:08 -0300

    refactor: refactor entry form component to extend base component

--- src/app/pages/entries/components/entry-form/entry-form.component.ts
+++ src/app/pages/entries/components/entry-form/entry-form.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, AfterContentChecked } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, AfterContentChecked, Injector } from '@angular/core';
+import { BaseFormComponent } from 'src/app/shared/components/base-form.component';
 
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { switchMap } from 'rxjs/operators';
+import { Validators } from '@angular/forms';
 
-import { MessageService } from 'primeng/api';
 import { EntriesService } from '../../services/entries-service/entries.service';
 import { CategoriesService } from 'src/app/pages/categories/services/categories-service/categories.service';
+
 import { Entry } from 'src/app/shared/models/entry.model';
 import { Category } from 'src/app/shared/models/category.model';
 
@@ -15,55 +14,39 @@ import { Category } from 'src/app/shared/models/category.model';
   templateUrl: './entry-form.component.html',
   styleUrls: ['./entry-form.component.scss']
 })
-export class EntryFormComponent implements OnInit, AfterContentChecked {
+export class EntryFormComponent extends BaseFormComponent<Entry> implements OnInit {
 
-  public entryForm: FormGroup;
-  public pageTitle: string;
-  public errorMessage: string;
-  public submitting: boolean = false;
-
-  public entry: Entry;
   public categories: Category[];
 
   public readonly MASK = { mask: Number, scale: 2, thousandsSeparator: '.', padFractionalZeros: true, NormalizeZeros: true, radix: ',' };
 
-  private currentAction: string;
-
-  private readonly TOAST = {severity: 'success', summary: 'Sucesso'};
-
   constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private entriesService: EntriesService,
-    private categoriesService: CategoriesService,
-    private messageService: MessageService,
-    private fb: FormBuilder
-  ) { }
-
-  ngOnInit() {
-    this.setCurrentAction();
-    this.buildEntryForm();
-    this.loadEntry();
-    this.loadCategories();
+    protected injector: Injector,
+    protected entriesService: EntriesService,
+    protected categoriesService: CategoriesService
+  ) {
+    super(
+      new Entry(),
+      injector,
+      entriesService,
+      Entry.fromData
+    );
   }
 
-  ngAfterContentChecked() {
-    this.setPageTitle();
+  ngOnInit() {
+    super.ngOnInit();
+    this.loadCategories();
   }
 
   // PUBLIC METHODS
 
   public submit(): void {
-    this.submitting = true;
-    if (this.currentAction === 'new') {
-      this.storeEntry();
-    } else {
-      this.updateEntry();
-    }
+    this.modelForm.get('amount').setValue(this.modelForm.get('amount').value.replace('.','').replace(',','.'));
+    super.submit();
   }
 
   public setPaid(paid: boolean): void {
-    this.entryForm.get('paid').setValue(paid);
+    this.modelForm.get('paid').setValue(paid);
   }
 
   public get typeOptions(): Array<any> {
@@ -76,18 +59,10 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
       });
   }
 
-  // PRIVATE METHODS
+  // PROTECTED METHODS
 
-  private setCurrentAction(): void {
-    if (this.route.snapshot.url[0].path === 'new') {
-      this.currentAction = 'new';
-    } else {
-      this.currentAction = 'edit';
-    }
-  }
-
-  private buildEntryForm(): void {
-    this.entryForm = this.fb.group({
+  protected buildModelForm(): void {
+    this.modelForm = this.fb.group({
       id: [null],
       name: [null, [Validators.required, Validators.minLength(3)]],
       description: [null],
@@ -99,57 +74,22 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
     });
   }
 
-  private loadEntry(): void {
-    if (this.currentAction === 'edit') {
-      this.route.paramMap.pipe(
-        switchMap(params => this.entriesService.getById(+params.get('id')))
-      ).subscribe((entry: Entry) => {
-        this.entry = entry;
-        this.entryForm.patchValue(entry);
-      });
-    }
-  }
-
-  private setPageTitle(): void {
-    const name = this.entry ? this.entry.name : '';
-
-    if (this.currentAction === 'new') {
-      this.pageTitle = 'Novo Lançamento';
-    } else {
-      this.pageTitle= `Atualizar Lançamento: ${name}`;
-    }
-  }
-
-  private loadCategories(): void {
+  protected loadCategories(): void {
     this.categoriesService.getAll().subscribe((categories: Category[]) => {
       this.categories = categories;
     });
   }
 
-  private storeEntry(): void {
-    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
-    entry.amount = entry.amount.replace('.','').replace(',','.');
-
-    this.entriesService.store(entry).subscribe((entry: Entry) => {
-      this.entryForm.reset();
-      this.successActions(entry);
-    });
+  protected setNewPageTitle(): string {
+    return 'Novo Lançamento';
   }
 
-  private updateEntry(): void {
-    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
-    entry.amount = entry.amount.replace('.','').replace(',','.');
-
-    this.entriesService.update(entry).subscribe((entry: Entry) => {
-      this.entryForm.reset();
-      this.successActions(entry);
-    });
+  protected setEditPageTitle(): string {
+    return `Atualizar Lançamento: ${this.model.name}`;
   }
 
-  private successActions(entry: Entry): void {
-    this.submitting = false;
-    this.messageService.add({...this.TOAST, detail: `Lançamento ${entry.name} salvo com sucesso!`});
-    this.router.navigate(['entries']);
+  protected setMessage(): string {
+    return 'Lançamento salvo com sucesso!';
   }
 
 }
